b87c0dcb99e7e983d391a7532efe3900e911da5c
bas-clavijo <bas.clavijo@duocuc.cl>
2023-02-22 05:52:00 -0300

    feat(type_hints):Edicion finalizada

--- type_hints.py
+++ type_hints.py
@@ -9,3 +9,18 @@ print(my_string_variable)
 print(type(my_string_variable))
 
 #Tipados de FastApi
+#se debe de especificar el tipo de la variable 
+#al especificar su tipo de dato nos daran diferentes funciones/(".")
+my_typed_variable: str = "My typed string variable "
+print(my_typed_variable)
+print(type(my_typed_variable))
+
+my_typed_variable: int = 5
+print(my_typed_variable)
+print(type(my_typed_variable))
+
+#tipado debil
+my_typed_variable: str = 5
+print(my_typed_variable)
+print(type(my_typed_variable))
+
