8a8dd76e4c23211ebee641bf9c0b4455a55ccde4
bas-clavijo <bas.clavijo@duocuc.cl>
2023-02-22 05:19:32 -0300

    feat(type_hints):Editando archivo

--- type_hints.py
+++ type_hints.py
@@ -1,2 +1,11 @@
-#Tips Hints
-#tipos de datos de FastApi
+#Tips Hints 
+#Tipados de Python
+my_string_variable = "My String variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 5
+print(my_string_variable)
+print(type(my_string_variable))
+
+#Tipados de FastApi
