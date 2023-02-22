824fe1e8d788f430a868157d4d1c0e2a30adee35
bas-clavijo <bas.clavijo@duocuc.cl>
2023-02-22 06:39:41 -0300

    feat(main):Editando archivo

--- FastApi/main.py
+++ FastApi/main.py
@@ -1,2 +1,12 @@
 #Primeros pasos
 #inicializar el modulo de FastApi
+from fastapi import FastAPI
+
+
+
+app = FastAPI() #Se crea una variable 
+
+#siempre que se quiera acceder a un servidor las funciones deben de ser asyncrona
+@app.get("/")
+async def root(): #se crea una funcion/operacion(funcion asyncrona)
+    return {"message": "Hello World"}
