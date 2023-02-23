e54539e5cac039264d9e79d1b99d3199e1d8cdc1
bas-clavijo <bas.clavijo@duocuc.cl>
2023-02-22 22:33:51 -0300

    Feat(main):Inicializando FastAPI

--- FastApi/main.py
+++ FastApi/main.py
@@ -7,6 +7,11 @@ from fastapi import FastAPI
 app = FastAPI() #Se crea una variable 
 
 #siempre que se quiera acceder a un servidor las funciones deben de ser asyncrona
+#se crea una funcion/operacion/peticiones(funcion asyncrona)
 @app.get("/")
-async def root(): #se crea una funcion/operacion(funcion asyncrona)
-    return {"message": "Hello World"}
+async def root(): 
+    return "Hello World"
+
+@app.get("/git")
+async def git():
+    return {"url": "https://github.com/bas-clavijo" }
