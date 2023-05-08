f1d57058955d46564df06a18da495e86be400886
bas-clavijo <bas.clavijo@duocuc.cl>
2023-05-08 01:39:52 -0400

    Creacion de usuario

--- FastApi/users.py
+++ FastApi/users.py
@@ -6,15 +6,25 @@ from pydantic import BaseModel
 app = FastAPI()
 
 #Entidad user
-class User():
+class User(BaseModel):
     name: str
     surname: str
     url: str
     age: int
 
+#Lista de usuarios
+users_list = [User(name="Bastian", surname="Clavijo",url="https://github.com/bas-clavijo", age=22),
+              User(name="Brais", surname="Moure",url="https://moure.dev", age=35),
+              User(name="Marco", surname="Tflay", url="https://github.com/Tflay", age=17)]
 
+#Clase que esta heredando un comportamiento de basemodel
 @app.get("/users")
-async def users(): 
+async def users():
+    return users_list
+
+#Entrada de forma manual
+@app.get("/usersjson")
+async def usersjson(): 
     #creacion de usarios
     return [{"name": "Bastian", "surname" : "Clavijo", "url": "https://github.com/bas-clavijo", "age": 22},
             {"name": "Brais", "surname" : "Moure", "url": "https://moure.dev", "age": 35},
