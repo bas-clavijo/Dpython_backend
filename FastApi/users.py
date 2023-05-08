a7cbc14aecf558f592d8a6c0fef0c1d90d149b84
bas-clavijo <bas.clavijo@duocuc.cl>
2023-05-08 01:14:17 -0400

    Creacion de usuario

--- FastApi/users.py
+++ FastApi/users.py
@@ -6,15 +6,19 @@ from pydantic import BaseModel
 app = FastAPI()
 
 #Entidad user
-
+class User():
+    name: str
+    surname: str
+    url: str
+    age: int
 
 
 @app.get("/users")
 async def users(): 
     #creacion de usarios
-    return [{"name": "Bastian", "surname" : "Clavijo", "url": "https://github.com/bas-clavijo"},
-            {"name": "Brais", "surname" : "Moure", "url": "https://moure.dev"},
-            {"name": "Marco", "surname" : "Tflay", "url": "https://github.com/Tflay"}]
+    return [{"name": "Bastian", "surname" : "Clavijo", "url": "https://github.com/bas-clavijo", "age": 22},
+            {"name": "Brais", "surname" : "Moure", "url": "https://moure.dev", "age": 35},
+            {"name": "Marco", "surname" : "Tflay", "url": "https://github.com/Tflay", "age": 17}]
 
 #inicializar el servidor uvicorn users:app --reload
 #prueba
