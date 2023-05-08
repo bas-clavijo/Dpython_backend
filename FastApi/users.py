8ef7dae258280cdebf5c596826c17851fce4005e
bas-clavijo <bas.clavijo@duocuc.cl>
2023-05-08 02:43:41 -0400

    Comprobacion de si la lista esta vacia

--- FastApi/users.py
+++ FastApi/users.py
@@ -7,15 +7,16 @@ app = FastAPI()
 
 #Entidad user
 class User(BaseModel):
+    id: int
     name: str
     surname: str
     url: str
     age: int
 
 #Lista de usuarios
-users_list = [User(name="Bastian", surname="Clavijo",url="https://github.com/bas-clavijo", age=22),
-              User(name="Brais", surname="Moure",url="https://moure.dev", age=35),
-              User(name="Marco", surname="Tflay", url="https://github.com/Tflay", age=17)]
+users_list = [User(id=1,name="Bastian", surname="Clavijo",url="https://github.com/bas-clavijo", age=22),
+              User(id=2,name="Brais", surname="Moure",url="https://moure.dev", age=35),
+              User(id=3,name="Marco", surname="Tflay", url="https://github.com/Tflay", age=17)]
 
 #Clase que esta heredando un comportamiento de basemodel
 @app.get("/users")
@@ -30,5 +31,15 @@ async def usersjson():
             {"name": "Brais", "surname" : "Moure", "url": "https://moure.dev", "age": 35},
             {"name": "Marco", "surname" : "Tflay", "url": "https://github.com/Tflay", "age": 17}]
 
+
+#Utilizacion del Path
+@app.get("/user/{id}")
+async def user(id: int):
+    users = filter(lambda user: user.id ==id, users_list)
+    #comprobacion de si la lista esta vacia
+    try:
+        return list(users)[0]
+    except:
+        return {"Error": "No se ha encontrado el usuario"}
+
 #inicializar el servidor uvicorn users:app --reload
-#prueba
