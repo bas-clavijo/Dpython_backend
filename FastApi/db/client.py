87831a813513b42a72e67249bcb25457d06151cb
bas-clavijo <bas.clavijo@duocuc.cl>
2023-05-11 20:05:13 -0400

    Configuracion de db en remoto

--- FastApi/db/client.py
+++ FastApi/db/client.py
@@ -1,4 +1,7 @@
 #Encargado de gestionar la conexion
 from pymongo import MongoClient
+#Base de datos local
+#db_client = MongoClient().local
 
-db_client = MongoClient()
+#base de datos remota
+db_client = MongoClient("mongodb+srv://basclavijo:bas.clavijo@cluster0.o3tomww.mongodb.net/?retryWrites=true&w=majority").basclavijo
